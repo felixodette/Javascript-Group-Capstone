b4b8990bad092b88f1f0f81d6a589e665cd5ebd5
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-05-31 09:31:51 +0430

    add the cards dynamicaly to the page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import Movie from './modules/data.js';
+import './style.css';
 
 const moviesObject = new Movie;
 
 const addDataToArrMovies = async () => {
-  const result = await moviesObject.getData()
-  console.log(result);
-  return result;
+  const arrTopMovies = await moviesObject.getData()
+  console.log(arrTopMovies);
+  return arrTopMovies;
 }
 
+const displayTopRatedMovies = async () => {
+  const topRatedMovies = await addDataToArrMovies();
+  const topRatedMoviesPart = document.getElementById('topRatedMovies');
+  topRatedMovies.forEach((movie) => {
+    const movieInfo = `<div class="top-rated-movie">
+    <img class="top-rated-movie-poster" src="${movie.poster}"></img>
+    <div class="top-rated-movie-title">${movie.title}</div>
+    <div class="top-rated-movie-release-date">${movie.releaseDate}</div>
+    </div>`;
+    topRatedMoviesPart.insertAdjacentHTML('beforeend', movieInfo);
+  });
+};
+
 window.addEventListener('DOMContentLoaded', async () => {
-  addDataToArrMovies();
+  await displayTopRatedMovies();
 });
