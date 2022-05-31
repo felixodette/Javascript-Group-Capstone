1f86c0963fd343ba714389e201f4825f6ee47d8d
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-01 02:02:44 +0430

    add the function to post data for the movie likes to the API

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,4 +1,5 @@
 const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=c7aa7fee798829f3624f83b8919fe2b9&language=en-US&page=1';
+const apiUrlForLikes = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/X3LACiofRMQXf8LGL8qW/likes/"
 
 export default class Movie {
   constructor(arrMovies) {
@@ -32,4 +33,30 @@ export default class Movie {
     this.addMoviesData(data);
     return this.arrMovies;
   };
+
+  addLikesToMovies = async (movieId) => {
+    const likesObj = {
+      item_id: movieId,
+    };
+    const response = await fetch(apiUrlForLikes, {
+      method: 'POST',
+      body: JSON.stringify(likesObj),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const responseResult = response.status;
+    if (responseResult === 201) {
+      return true;
+    };
+  };
+
+  getMoviesLike = async () => {
+    const response = await fetch(apiUrlForLikes, {
+      method: 'GET',
+    });
+    const movieLikes = await response.json();
+    console.log(movieLikes);
+    return movieLikes;
+  }
 }
