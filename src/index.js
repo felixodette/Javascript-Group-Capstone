a4792fa5461bafeda7c49cdcbb3fd9f8b27b596b
Arturgouveia1970 <artgouveiag@gmail.com>
2022-06-01 19:40:46 +0100

    linters corections

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ const displayTopRatedMovies = async () => {
           </form>
         </div>
         
-        </div>`
+        </div>`;
     topRatedMoviesPart.insertAdjacentHTML('beforeend', movieInfo);
   });
 };
@@ -73,42 +73,3 @@ window.addEventListener('DOMContentLoaded', async () => {
     } */
   });
 });
-
-/*<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog" role="document">
-  <div class="modal-content">
-    <div class="modal-header">
-      <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <form>
-        <div class="form-group">
-          <label for="recipient-name" class="col-form-label">Recipient:</label>
-          <input type="text" class="form-control" id="recipient-name">
-        </div>
-        <div class="form-group">
-          <label for="message-text" class="col-form-label">Message:</label>
-          <textarea class="form-control" id="message-text"></textarea>
-        </div>
-      </form>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      <button type="button" class="btn btn-primary">Send message</button>
-    </div>
-  </div>
-</div>
-</div>*/
-
-/*<input class="username" type="text" placeholder="enter name">
-            <input class="
-            <h4 class="text-light">Comments</h4>
-            
-            <!-- comments table -->
-            <table class="table table-hover text-light">
-              <thead>
-              <tr>
-                  <th scope="col">#</th> */
