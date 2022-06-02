276b1c7d2fc7f2712a0edc2991ce92f5e5236187
Arturgouveia1970 <artgouveiag@gmail.com>
2022-06-02 20:45:00 +0100

    intoduced popup module

--- src/index.js
+++ src/index.js
@@ -20,9 +20,35 @@ const displayTopRatedMovies = async () => {
     <p class="top-rated-movie-release-date light-color h6">${movie.releaseDate}</p>
     <div class="like-comment d-flex justify-content-between align-items-center">
     <p class="text-danger h6 show-likes-part p-1 m-0" id="showLikes${movie.id}">${movie.like}</p><i class="fa-regular fa-heart light-color" id="like${movie.id}">
-    </i><i class="fa-regular fa-comment light-color" id="comment${movie.id}"></i><i class="fa-regular fa-calendar light-color" id="reservation${movie.id}"></i>
-    </div>
-    </div>`;
+    </i>
+    <a id="comment" data-bs-toggle="modal" data-bs-target="#staticBackdrop" data-comments><i class="fa-regular fa-comment light-color" id="comments${movie.id}"></i></a>
+
+    <div class="modal fade modal-dialog-scrollable" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="text-center" id="staticBackdropLabel">${movie.title}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="img-div"><img class="top-rated-movie-poster-modal" src="${movie.poster}" alt=""></div>
+            <article class="text-center">Release Date: ${movie.releaseDate} ID: ${movie.id}</article>
+          </div>
+          <form>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">Enter your name:</label>
+              <input type="text" class="form-control" id="recipient-name">
+            </div>
+            <div class="form-group">
+              <label for="message-text" class="col-form-label">Your comments:</label>
+              <textarea class="form-control" id="message-text"></textarea>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Send message</button>
+            </div>
+          </form>
+        </div>`;
     topRatedMoviesPart.insertAdjacentHTML('beforeend', movieInfo);
   });
 };
---
+++ src/modules/popUp.js
@@ -0,0 +1,39 @@
+const addPopUpToMoviesDiv = () => {
+  const popUpPart = document.getElementById('popUp');
+  popUpPart.innerHTML = `
+  <div class="modal fade modal-dialog-scrollable" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="text-center" id="staticBackdropLabel">Movie Information</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body p-4 d-flex flex-column align-items-center">
+          <p id="popUpMovieId" class="align-self-start"></p>
+          <div class="row">
+            <div class="col-6" id="popUpPoster"></div>
+            <div class="col-6 pt-5">
+              <h2 class="text-center h2 mt-5" id="popUpMovieTitle"></h2>
+              <p id="popUpMovieDescription"></p>
+            </div>
+          </div>
+        </div>
+        <form id="commentForm" class="p-4">
+          <div class="form-group">
+            <label for="user-name" class="col-form-label">Enter your name:</label>
+            <input type="text" class="form-control" id="user-name">
+          </div>
+          <div class="form-group">
+            <label for="user-comment" class="col-form-label">Your comments:</label>
+            <textarea class="form-control" id="user-comment"></textarea>
+          </div>
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-dark">Send comment</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>`;
+};
+
+export { addPopUpToMoviesDiv };
