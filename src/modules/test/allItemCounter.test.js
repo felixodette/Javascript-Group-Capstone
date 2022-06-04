d58cad6f81c90dd2516ab91bf97274f4f443b0b3
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-04 13:55:06 +0430

    add the test for all item counter function

---
+++ src/modules/test/allItemCounter.test.js
@@ -0,0 +1,8 @@
+import { calculateIteme } from "../counters/moviesCounter";
+
+const arrMovie = [{ movie: 1 }, { movie: 2 }, { movie: 3 }, { movie: 4 }, { movie: 5 }];
+
+test('Add function', () => {
+  const result = calculateIteme(arrMovie);
+  expect(result).toBe(5);
+});
