6410bebffd5ada1309778e2b534470f5b97ccb39
Felix Odete <felix.odette@gmail.com>
2022-06-04 12:48:24 +0300

    Reservation counter test

---
+++ src/modules/test/reservations.test.js
@@ -0,0 +1,14 @@
+import { calculateReservations } from "../counters/reservationsCounter";
+
+const arrMovie = [
+  { user: 1, start_date: '...', end_date: '...' },
+  { user: 2, start_date: '...', end_date: '...' },
+  { user: 3, start_date: '...', end_date: '...' },
+  { user: 4, start_date: '...', end_date: '...' },
+  { user: 5, start_date: '...', end_date: '...' },
+  { user: 6, start_date: '...', end_date: '...' }];
+
+test('Add function', () => {
+  const result = calculateReservations(arrMovie);
+  expect(result).toBe(6);
+});
